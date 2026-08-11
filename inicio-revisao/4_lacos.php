b7583d87ed73bd2e66d084607c3b3003eb936304
sahcamilo <sarahxgames@gmail.com>
2026-08-11 08:42:48 -0300

    revisão.2
    Continuação da revisão

--- inicio-revisao/4_lacos.php
+++ inicio-revisao/4_lacos.php
@@ -2,5 +2,22 @@
 
 // Laço (FOR) para Tabuada 8
 for ($i =1; $i <=10; $i++) {
-    echo "8 x $i = " . (8 * $i) . "\n";
+    echo "8 x $i = " . (8 * $i) . "<br>";
 } 
+
+// While -(enquanto) Contagem regressiva
+echo "<br>";
+$n = 5;
+while($n >0) {
+    echo $n . '<br>';
+    $n--;
+}
+
+// Do while - (faça enquanto) Exwcuta ao menos 1vez
+
+echo "<br>";
+$x = 0;
+do{
+    echo "x vale: $x <br>";
+    $x++;
+} while ($x <= 10);
